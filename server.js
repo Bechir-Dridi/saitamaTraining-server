e622e7c67ea9d9918a2986193149dcf2703c97ae
Bechir-Dridi <99827725+Bechir-Dridi@users.noreply.github.com>
2023-08-16 15:15:09 +0100

    cors fix 3

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ const app = express()
 //middleware:
 
 // Middleware to enable CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors(
+    { origin: ["http://localhost:3000", "https://saitama-server.onrender.com"], credentials: true, } //server accepts requests from loclahost 3000 and static site
+))
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
