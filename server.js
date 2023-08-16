2be040ab4edbec484c418076d0e99910de8135c5
Bechir-Dridi <99827725+Bechir-Dridi@users.noreply.github.com>
2023-08-16 17:53:27 +0100

    cors fix 12

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://bdev-saitama.netlify.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
