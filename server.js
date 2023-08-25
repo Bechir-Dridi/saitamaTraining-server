89b082978e9ce9d770e63db9a73d3bfa8da1df8f
Bechir-Dridi <99827725+Bechir-Dridi@users.noreply.github.com>
2023-08-25 21:36:39 +0100

    signature added to server

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// developed by "Bechir Dridi"
+// Portfolio: https://bechirdev.netlify.app
+// twitter:   https://twitter.com/bechir7dridi
+// linkedin:  https://linkedin.com/in/bechir-dev/
+// github:    https://github.com/Bechir-Dridi
 const express = require("express")
 require("dotenv").config()
 const workoutRoutes = require("./routes/workouts")
